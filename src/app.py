2c905d8a8ae94d30a3b6b3bcfcbc533ab4353ce7
Pablo Robles <156269638+PabloRoblesA@users.noreply.github.com>
2024-06-03 22:58:46 +0000

    #1

--- src/app.py
+++ src/app.py
@@ -1,5 +1,3 @@
-from flask import Flask, request, render_template
-from pickle import load
+import streamlit as st
 
-app = Flask(__name__)
-model = load(open("../models/random_forest_classifier_default_42.sav", "rb"))
+st.title("Hello, World!")
