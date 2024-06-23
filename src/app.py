9ba669929521174be6cffbf01f5c0086bcad97ec
PabloRoblesA <ajrobles@hotmail.es>
2024-06-23 19:38:08 -0400

    #cas

--- src/app.py
+++ src/app.py
@@ -4,15 +4,15 @@ import streamlit as st
 model = load(open("../models/logistic_regression_default.sav", "rb"))
 
 class_dict = {
-    "True": "Es un pokemon LEGENDARIO, atrapalo!",
-    "False": "Mejor suerte la proxima, no es LEGENDARIO.",
+    "False": "Mejor suerte la proxima, no es LEGENDARIO",
+    "True": "Es un pokemon LEGENDARIO, atrapalo!"
 }
 
 st.title("Prediccion de legendarios")
 
-val = st.slider("Total", min_value=0, max_value=750, step=1)
+val = st.slider("Total", min_value=10, max_value=780, step=1)
 
-if st.button("Quien es ese pokeon?"):
+if st.button("Quien es ese pokemon?"):
 
     prediction = str(model.predict([[val]])[0])
     pred_class = class_dict[prediction]
