a9cdec60c5efd76579a5397ff594746389088249
PabloRoblesA <ajrobles@hotmail.es>
2024-06-23 19:59:28 -0400

    #casino

--- src/app.py
+++ src/app.py
@@ -1,19 +1,25 @@
 from pickle import load
 import streamlit as st
 
-model = load(open("../models/logistic_regression_default.sav", "rb"))
+model = load(open("../models/logistic_regression_default_42.sav", "rb"))
 
 class_dict = {
-    "False": "Mejor suerte la proxima, no es LEGENDARIO",
-    "True": "Es un pokemon LEGENDARIO, atrapalo!"
+    "0": "Iris setosa",
+    "1": "Iris versicolor",
+    "2": "Iris virginica"
 }
 
 st.title("Prediccion de legendarios")
-
-val = st.slider("Total", min_value=10, max_value=780, step=1)
+val1 = st.slider("Total", min_value=180, max_value=780, step=1)
+val2 = st.slider("HP", min_value=1, max_value=255, step=1)
+val3 = st.slider("Attack", min_value=1, max_value=255, step=1)
+val4 = st.slider("Defense", min_value=1, max_value=255, step=1)
+val5 = st.slider("Sp. Atk", min_value=1, max_value=255, step=1)
+val6 = st.slider("Sp. Def", min_value=1, max_value=255, step=1)
+val7 = st.slider("Speed", min_value=1, max_value=255, step=1)
 
 if st.button("Quien es ese pokemon?"):
-
-    prediction = str(model.predict([[val]])[0])
+    prediction = str(model.predict(
+        [[val1, val2, val3, val4, val5, val6, val7]])[0])
     pred_class = class_dict[prediction]
     st.write("Quien es ese pokemon?:", pred_class)
