f7d2a7c8611ec9137ee992d93d9e7eb61d0cedd3
PabloRoblesA <ajrobles@hotmail.es>
2024-06-23 19:19:13 -0400

    #casi casi

--- src/app.py
+++ src/app.py
@@ -1,3 +1,19 @@
+from pickle import load
 import streamlit as st
 
-st.title("Hello, World!")
+model = load(open("../models/logistic_regression_default.sav", "rb"))
+
+class_dict = {
+    "True": "Es un pokemon LEGENDARIO, atrapalo!",
+    "False": "Mejor suerte la proxima, no es LEGENDARIO.",
+}
+
+st.title("Prediccion de legendarios")
+
+val = st.slider("Total", min_value=0, max_value=750, step=1)
+
+if st.button("Predict"):
+
+    prediction = str(model.predict([[val]])[0])
+    pred_class = class_dict[prediction]
+    st.write("Prediction:", pred_class)
