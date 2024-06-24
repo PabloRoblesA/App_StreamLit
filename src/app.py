b4544e524b08f0ba96ae4ff80fa7a2bba60cfe31
PabloRoblesA <ajrobles@hotmail.es>
2024-06-23 20:29:02 -0400

    #3

--- src/app.py
+++ src/app.py
@@ -1,18 +1,22 @@
 from pickle import load
 import streamlit as st
+import numpy as np
 
 model = load(open("../models/logistic_regression_default_42.sav", "rb"))
+scaler = load(open("../models/scaler.sav", "rb"))
 
 class_dict = {
-    "True": "Es",
-    "False": "No es",
+    "True": "Es un pokemon LEGENDARIO, atrapalo si puedes!",
+    "False": "No es un LEGENDARIO, buena suerte la proxima vez.",
 }
 
 st.title("Prediccion de legendarios")
 val1 = st.slider("Total", min_value=180, max_value=780, step=1)
 
 if st.button("Quien es ese pokemon?"):
-    prediction = str(model.predict(
-        [[val1]])[0])
+    features = np.array([[val1]])
+    scaled_features = scaler.transform(features)
+
+    prediction = model.predict(scaled_features)[0]
     pred_class = class_dict[prediction]
     st.write("Quien es ese pokemon?:", pred_class)
