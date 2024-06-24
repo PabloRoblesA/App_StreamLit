f08f3c8bfea60122150df1fdf5c17d41fee5ee21
PabloRoblesA <ajrobles@hotmail.es>
2024-06-23 21:03:04 -0400

    #last

--- src/app.py
+++ src/app.py
@@ -12,7 +12,7 @@ class_dict = {
 
 st.title("Prediccion de legendarios")
 val1 = st.slider("Total", min_value=1, max_value=780, step=1)
-val2 = st.slider("HP", min_value=1, max_value=780, step=1)
+val2 = st.slider("HP", min_value=1, max_value=255, step=1)
 val3 = st.slider("Attack", min_value=1, max_value=780, step=1)
 val4 = st.slider("Defense", min_value=1, max_value=780, step=1)
 val5 = st.slider("Sp. Atk", min_value=1, max_value=780, step=1)
@@ -20,7 +20,7 @@ val6 = st.slider("Sp. Def", min_value=1, max_value=780, step=1)
 val7 = st.slider("Speed", min_value=1, max_value=780, step=1)
 
 if st.button("Quien es ese pokemon?"):
-    features = np.array([[val1]])
+    features = np.array([[val1, val2, val3, val4, val5, val6, val7]])
     scaled_features = scaler.transform(features)
 
     prediction = model.predict(scaled_features)[0]
